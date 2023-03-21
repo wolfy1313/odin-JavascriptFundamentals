e14d10e99498bf6b9aae25125b77250b7eae32ee
jeremyHarrell <hillvulf@gmail.com>
2023-03-21 13:03:28 -0500

    finish mdn lesson

--- javascript.js
+++ javascript.js
@@ -18,3 +18,24 @@
 // let ourPlanet = "Earth";
 // let currentVisitor = "Barth"
 
+const myInt = 5;
+const myFloat = 6.667;
+myInt;
+myFloat;
+console.log(typeof myInt);
+console.log(typeof myFloat) ;
+
+const lotsOfDecimals = 1.78935982375948234;
+const twoDecimalPlaces = lotsOfDecimals.toFixed(2)
+console.log(twoDecimalPlaces)
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3
+console.log(myNumber)
+myNumber += 3
+console.log(myNumber)
+
+myNumber++
+console.log(myNumber)
+myNumber--
+console.log(myNumber)
