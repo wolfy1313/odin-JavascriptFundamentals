f1fca9fc8f4b4a2ce6862d747d5b32d08c43ae15
jeremyHarrell <hillvulf@gmail.com>
2023-03-21 16:55:05 -0500

    finish second javascript.info lesson on operators

--- javascript.js
+++ javascript.js
@@ -18,24 +18,66 @@
 // let ourPlanet = "Earth";
 // let currentVisitor = "Barth"
 
-const myInt = 5;
-const myFloat = 6.667;
-myInt;
-myFloat;
-console.log(typeof myInt);
-console.log(typeof myFloat) ;
+// const myInt = 5;
+// const myFloat = 6.667;
+// myInt;
+// myFloat;
+// console.log(typeof myInt);
+// console.log(typeof myFloat) ;
 
-const lotsOfDecimals = 1.78935982375948234;
-const twoDecimalPlaces = lotsOfDecimals.toFixed(2)
-console.log(twoDecimalPlaces)
+// const lotsOfDecimals = 1.78935982375948234;
+// const twoDecimalPlaces = lotsOfDecimals.toFixed(2)
+// console.log(twoDecimalPlaces)
 
-let myNumber = "74";
-myNumber = Number(myNumber) + 3
-console.log(myNumber)
-myNumber += 3
-console.log(myNumber)
+// let myNumber = "74";
+// myNumber = Number(myNumber) + 3
+// console.log(myNumber)
+// myNumber += 3
+// console.log(myNumber)
 
-myNumber++
-console.log(myNumber)
-myNumber--
-console.log(myNumber)
+// myNumber++
+// console.log(myNumber)
+// myNumber--
+// console.log(myNumber)
+
+// let a=1, b=1;
+
+// let c = ++a;
+// console.log(c)
+// let d = b++;
+// console.log("d equals " + d)
+
+// "" + 1 + 0
+// "10"
+// "" - 1 + 0
+// -1
+// true + false
+// 1
+// 6 / "3"
+// 2
+// "2" * "3"
+// 6
+// 4 + 5 + "px"
+// "9px"
+// "$" + 4 + 5
+// "$45"
+// "$"
+// "4" - 2
+// 2
+// "4px" - 2
+// NaN
+// " -9 " + 5
+// " -9 5"
+// " -9 " - 5
+// -14
+// null + 1
+// 1
+// undefined + 1
+// NaN
+// " \t \n" - 2
+// -2
+
+let a = prompt("First number?", 1)
+let b = prompt("Second number?", 2)
+
+alert(+a + +b)
