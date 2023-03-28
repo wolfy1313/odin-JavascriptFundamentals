40df031ea4fe6ca0d5eb319b25eff5a82c7f69b0
jeremyHarrell <hillvulf@gmail.com>
2023-03-28 08:47:37 -0500

    mdn odin lesson

--- javascript.js
+++ javascript.js
@@ -261,3 +261,13 @@
 
 const myNumber = Math.random();
 console.log(myNumber)
+
+function draw() {
+  ctx.clearRect(0,0,WIDTH,HEIGHT);
+  for (let i = 0; i < 100; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,0,0,0.5)';
+    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
