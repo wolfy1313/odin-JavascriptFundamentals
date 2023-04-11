995792f342e223ac83b766ea2f5eafc33a460069
jeremyHarrell <hillvulf@gmail.com>
2023-04-11 12:52:08 -0500

    mdn arrow function lesson

--- javascript.js
+++ javascript.js
@@ -259,31 +259,36 @@
 // const madeAString = myArray.join(' ');
 // console.log(madeAString);
 
-const btn = document.querySelector('button');
-    const canvas = document.querySelector('canvas');
-    const ctx = canvas.getContext('2d');
+// const btn = document.querySelector('button');
+//     const canvas = document.querySelector('canvas');
+//     const ctx = canvas.getContext('2d');
 
-    let WIDTH = document.documentElement.clientWidth;
-    let HEIGHT = document.documentElement.clientHeight;
+//     let WIDTH = document.documentElement.clientWidth;
+//     let HEIGHT = document.documentElement.clientHeight;
 
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
+//     canvas.width = WIDTH;
+//     canvas.height = HEIGHT;
 
-    function random(number) {
-      return Math.floor(Math.random()*number);
-    }
+//     function random(number) {
+//       return Math.floor(Math.random()*number);
+//     }
 
-const myNumber = Math.random(5);
-console.log(myNumber)
+// const myNumber = Math.random(5);
+// console.log(myNumber)
 
-function draw() {
-  ctx.clearRect(0,0,WIDTH,HEIGHT);
-  for (let i = 0; i < 100; i++) {
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(255,0,0,0.5)';
-    ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
-    ctx.fill();
-  }
-}
-btn.addEventListener('click', draw)
-draw()
+// function draw() {
+//   ctx.clearRect(0,0,WIDTH,HEIGHT);
+//   for (let i = 0; i < 100; i++) {
+//     ctx.beginPath();
+//     ctx.fillStyle = 'rgba(255,0,0,0.5)';
+//     ctx.arc(random(WIDTH), random(HEIGHT), random(50), 0, 2 * Math.PI);
+//     ctx.fill();
+//   }
+// }
+// btn.addEventListener('click', draw)
+// draw()
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener('keydown', (event) => output.textContent = `You pressed "${event.key}".`);
